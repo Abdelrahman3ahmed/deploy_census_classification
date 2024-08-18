5a0fed0a2f2553777170f90796df91b24b9eae77
abdelrahman.ragab <abdelrahman.ragab@cyshield.com>
2024-08-18 16:59:25 +0300

    ignore models

--- starter/train_model.py
+++ starter/train_model.py
@@ -1,28 +1,28 @@
-# Script to train machine learning model.
-
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+import joblib
 
-# Add the necessary imports for the starter code.
+def train_model(input_path: str, model_path: str, encoders_path: str):
+    data = pd.read_csv(input_path)
+    
+    label_encoders = {}
+    for column in data.select_dtypes(include=['object']).columns:
+        le = LabelEncoder()
+        data[column] = le.fit_transform(data[column])
+        label_encoders[column] = le
 
-# Add code to load in the data.
+    X = data.drop('income', axis=1)
+    y = data['income']
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Optional enhancement, use K-fold cross validation instead of a train-test split.
-train, test = train_test_split(data, test_size=0.20)
+    model = RandomForestClassifier()
+    model.fit(X_train, y_train)
+    
+    joblib.dump(model, model_path)
+    joblib.dump(label_encoders, encoders_path)
 
-cat_features = [
-    "workclass",
-    "education",
-    "marital-status",
-    "occupation",
-    "relationship",
-    "race",
-    "sex",
-    "native-country",
-]
-X_train, y_train, encoder, lb = process_data(
-    train, categorical_features=cat_features, label="salary", training=True
-)
+if __name__ == "__main__":
+    train_model('data/clean_census.csv', 'models/model.joblib', 'models/label_encoders.joblib')
 
-# Proces the test data with the process_data function.
-
-# Train and save a model.
