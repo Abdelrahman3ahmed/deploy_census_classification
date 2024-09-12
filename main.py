ad9dd564c12280f5aa3c67dedc9b672f77ac6d7c
abdelrahman.ragab <abdelrahman.ragab@cyshield.com>
2024-09-13 01:31:40 +0300

    Procfile

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 # Put the code for your API here.
 import uvicorn
+import os
 from src.api import app
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    port = int(os.getenv("PORT", 8000))  
+    uvicorn.run(app, host="0.0.0.0", port=port)
+
 
